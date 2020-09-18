3c2a60aa61f957a1cf70a7db9544515d85187a61
Captainistz- <61690373+Captainistz@users.noreply.github.com>
2020-09-18 22:19:16 +0700

    + Readme + Clean Codes

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 
 Hi! This project was created for SMTE Project Contest. Still in working! 😁
 
-This project using 
-- OpenCV
-- Tesseract OCR (Thai)
-- LINE Notify
-- MySQL
+## About
+This project can be detected and read Thai license plate using [Image Processing](https://en.wikipedia.org/wiki/Digital_image_processing) and [Optical Character Recognition](https://en.wikipedia.org/wiki/Optical_character_recognition)
+
+## Requirements 
+- [OpenCV](https://pypi.org/project/opencv-contrib-python/)
+- [Tesseract OCR (Thai)](https://pypi.org/project/pytesseract/)
+- [Numpy](https://pypi.org/project/numpy/)
+- [LINE Notify](https://developers.line.biz/en/)
+- [MySQL](https://pypi.org/project/mysql-connector-python/)
+
+
+## Usage
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
+import os
 import cv2
 import time
+import urllib
 import imutils
 import _database
 import pytesseract
 import numpy as np
 import mysql.connector
+from dotenv import load_dotenv
+
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-vid = cv2.VideoCapture(0)
+#vid = cv2.VideoCapture(0)
+
+load_dotenv()
 
 while 1:
-    ret, img = vid.read()
+    #ret, img = vid.read()
+    url = os.getenv('CAMERA_URL')
+
+    imgResp = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    img = cv2.imdecode(imgNp,-1)
+
     og_im = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
     edged = cv2.Canny(gray, 100, 200)
+    
     contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
@@ -39,13 +52,13 @@ while 1:
         (topx, topy) = (np.min(x), np.min(y))
         (bottomx, bottomy) = (np.max(x), np.max(y))
         Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-
+        
         text = pytesseract.image_to_string(Cropped, 'tha')
         _database.check_plate(text, og_im)
-
+        #print(text)
         text = pytesseract.image_to_string(img, 'tha')
         _database.check_plate(text, og_im)
-
+        #   print(text)
         Cropped = cv2.resize(Cropped, (400, 200))
         cv2.imshow('Cropped', Cropped)
 
@@ -54,5 +67,5 @@ while 1:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-vid.release()
+#vid.release()
 cv2.destroyAllWindows()
